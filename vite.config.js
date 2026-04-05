480be96806aa15cd78cdb7a2b559b14ca1ec1b15
Andrea <andrea@example.com>
2026-04-05 20:45:12 +0800

    latest fixes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 6000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'sanity-vendor': ['sanity', '@sanity/client', '@sanity/icons'],
+          'lucide-vendor': ['lucide-react'],
+        }
+      }
+    }
+  }
 })
